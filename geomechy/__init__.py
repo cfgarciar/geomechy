84cfdcdfe402b8975e07d2c66ea8cd034b00093e
cfgarciar <cfgarciar@unal.edu.co>
2020-10-06 23:27:56 -0500

    notebooks update

--- geomechy/__init__.py
+++ geomechy/__init__.py
@@ -3,3 +3,6 @@ __version__ = "1.0.1"
 from geomechy.core import *
 from geomechy.physics import *
 from geomechy.base import *
+from geomechy.utils import *
+from geomechy.io import *
+from geomechy.materials import *
